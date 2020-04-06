1738ed76172a0b46570fcc0f4da11fd0aa869269
brkline <8756612+brkline@users.noreply.github.com>
2020-04-05 21:34:40 -0400

    Started building JS file

--- udacity/landing-page/js/app.js
+++ udacity/landing-page/js/app.js
@@ -11,30 +11,50 @@
  * 
  * JS Standard: ESlint
  * 
-*/
+ */
 
 /**
  * Define Global Variables
  * 
-*/
-
+ */
+let sectionList = [];
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
-*/
+ */
+function setActiveSection() {
+    aObj = document.getElementById('nav').getElementsByTagName('a');
+    for (i = 0; i < aObj.length; i++) {
+        if (document.location.href.indexOf(aObj[i].href) >= 0) {
+            aObj[i].className = 'active';
+        }
+    }
+}
 
+function getSectionList() {
+    const sections = document.getElementsByTagName('section');
+    for (section of sections) {
+        sectionList.push(section.getAttribute('data-nav'));
+    };
 
+    return sectionList;
+}
 
 /**
  * End Helper Functions
  * Begin Main Functions
  * 
-*/
-
+ */
+console.log(getSectionList());
 // build the nav
-
+for (let listItem of sectionList){
+    const newListItem = document.createElement('li');
+    const newTextElement = document.createTextNode(listItem);
+    const navBarList = document.getElementById('navbar__list');
+    newListItem.appendChild(newTextElement);
+}
 
 // Add class 'active' to section when near top of viewport
 
@@ -46,12 +66,10 @@
  * End Main Functions
  * Begin Events
  * 
-*/
+ */
 
 // Build menu 
 
 // Scroll to section on link click
 
 // Set sections as active
-
-
