3a87b3f948af9c57b8058ac4606e607b10b8f6a5
brkline <8756612+brkline@users.noreply.github.com>
2020-04-10 14:33:16 -0400

    Finished up active scroll

--- udacity/landing-page/js/app.js
+++ udacity/landing-page/js/app.js
@@ -38,37 +38,12 @@ function setActiveSection(event) {
     const eventTarget = event.currentTarget.innerText;
     const eventType = event.type;
     const sections = document.getElementsByTagName('section');
-    // const listItem = document.querySelector('.active');
-    // listItem.className = 'menu__link'
-    // let currentActiveSection = document.getElementsByClassName('your-active-class');
-    // currentActiveSection[0].removeAttribute('class');
-    // let newActive = eventTarget;
-    // Code below is based on answers found here:
-    // https://knowledge.udacity.com/questions/66312
-    // https://knowledge.udacity.com/questions/85408#96950
 
     if (eventType == 'click') {
-        // let sectionId = eventTarget.split(' ');
-        // sectionId[0] = sectionId[0].toLowerCase();
-        // let newActive = document.getElementById(`${sectionId[0]}${sectionId[1]}`);
-        // if (newActive.className = '') {
-        //     newActive.className += ' your-active-class';
-        // } else {
-        //     newActive.className = 'your-active-class';
-        // }
-        // for (let section of sections) {            
-        //     if (section.getAttribute('data-nav') == eventTarget) {
-        //         if (section.className = '') {
-        //             section.className += ' your-active-class';                                
-        //         } else {
-        //             section.className = 'your-active-class';                
-        //         }            
-        //         eventTarget.className = 'menu__link active';
-        //     } else {
-        //         section.removeAttribute('class');
-
-        //     }
-        // }      
+        // Code below is based on answers found here:
+        // https://knowledge.udacity.com/questions/66312
+        // https://knowledge.udacity.com/questions/85408#96950
+        let newActive = '';
         for (const section of sections) {
             const menuItem = document.getElementsByClassName(section.id);
             if (section.className.includes('your-active-class')) {
@@ -78,27 +53,11 @@ function setActiveSection(event) {
             if (section.getAttribute('data-nav') == eventTarget) {
                 section.className = 'your-active-class';
                 menuItem.item(0).className = ''.concat('menu__link ', section.id, ' active');
+                newActive = section.id;
             }
         }
-        // for (const item of document.getElementsByClassName('menu__link')){
-        //     const className = item.className;
-        //     const id = item.id;
-        //     // const menuItem = document.querySelector(''.concat('.', item.className));
-        //     const section = document.getElementById(item.id)
-        //     if (item.className.includes('active')){
-        //         item.className = ''.concat('menu__link ', item.id);
-        //         section.removeAttribute('class');            
-        //     }
-        //     if (item.innerHTML = eventTarget) {
-        //         item.className += ' active';
-        //         section.className = 'your-active-class';
-        //     }
-
-        // }
-        // const listItem = document.querySelector('.active');
-        // listItem.className = 'menu__link'
-        scrollTo(eventTarget);
-    } else if (eventType == 'scroll') {
+        scrollTo(newActive);
+    } else if (eventType == 'scroll' && previousEvent != 'click') {
         for (const section of sections) {
             const box = section.getBoundingClientRect();
             const menuItem = document.getElementsByClassName(section.id);
@@ -112,80 +71,79 @@ function setActiveSection(event) {
                     menuItem.item(0).className = ''.concat('menu__link ', section.id);
                 }
             }
-        }       
+        }
     }
     // Save the eventType so that if this function is invoked again,
     // we can perform actions based on this value.
     previousEvent = eventType;
 }
 
-    /**
-     * Gets a list of existing sections on the page.
-     *  
-     */
-    function getSectionList() {
-        const sections = document.getElementsByTagName('section');
-        let sectionList = [];
-        sectionIdList = [];
-        for (let section of sections) {
-            sectionList.push(section.getAttribute('data-nav'));
-            const sectionName = section.getAttribute('data-nav');
-            let sectionId = ''.concat(sectionName.slice(0, 1).toLowerCase(), sectionName.replace(/ /g, '').slice(1));
-            sectionIdList.push(sectionId);
-        };
+/**
+ * Gets a list of existing sections on the page.
+ *  
+ */
+function getSectionList() {
+    const sections = document.getElementsByTagName('section');
+    let sectionList = [];
+    sectionIdList = [];
+    for (let section of sections) {
+        sectionList.push(section.getAttribute('data-nav'));
+        const sectionName = section.getAttribute('data-nav');
+        let sectionId = ''.concat(sectionName.slice(0, 1).toLowerCase(), sectionName.replace(/ /g, '').slice(1));
+        sectionIdList.push(sectionId);
+    };
 
-        return sectionList;
-    }
+    return sectionList;
+}
 
 
-    /**
-     * Scrolls to the active section.
-     * 
-     */
-    function scrollTo(newActive) {
-        // const sectionToScroll = document.getElementById(newActive.id);    
-        const sections = document.getElementsByTagName('section');
-        for (let section of sections) {
-            if (section.getAttribute('data-nav') == newActive) {
-                section.scrollIntoView({
-                    behavior: 'smooth',
-                    block: 'end'
-                });
-            }
+/**
+ * Scrolls to the active section.
+ * 
+ */
+function scrollTo(newActive) {
+    const sections = document.getElementsByTagName('section');
+    for (let section of sections) {
+        if (section.id == newActive) {
+            section.scrollIntoView(true);
+            section.scrollIntoView({
+                behavior: 'smooth',
+                block: 'end'
+            });
         }
     }
+}
 
-    /**
-     * Processes the list of sections and builds the Navigation Menu
-     * 
-     */
-    function buildNavMenu(sectionList) {
-        for (let [i, sectionName] of sectionList.entries()) {
-            const newListItem = document.createElement('li');
-            const newTextElement = document.createTextNode(sectionName);
-            // If it's the first menu item, we'll set it as active.
-            // Otherwise, we'll just added a single class.
-            let workClassName = ''.concat('menu__link ', sectionIdList[i]);
-            if (i == 0) {
-                workClassName += ' active';
-            }
-            newListItem.setAttribute('class', workClassName);
-            // newListItem.setAttribute('id', sectionIdList[i]);
-            newListItem.appendChild(newTextElement);
-            const navBarList = document.querySelector('#navbar__list');
-            navBarList.appendChild(newListItem);
-            newListItem.addEventListener('click', setActiveSection);
+/**
+ * Processes the list of sections and builds the Navigation Menu
+ * 
+ */
+function buildNavMenu(sectionList) {
+    for (let [i, sectionName] of sectionList.entries()) {
+        const newListItem = document.createElement('li');
+        const newTextElement = document.createTextNode(sectionName);
+        // If it's the first menu item, we'll set it as active.
+        // Otherwise, we'll just added a single class.
+        let workClassName = ''.concat('menu__link ', sectionIdList[i]);
+        if (i == 0) {
+            workClassName += ' active';
         }
-        document.addEventListener('scroll', setActiveSection);
+        newListItem.setAttribute('class', workClassName);
+        newListItem.appendChild(newTextElement);
+        const navBarList = document.querySelector('#navbar__list');
+        navBarList.appendChild(newListItem);
+        newListItem.addEventListener('click', setActiveSection);
     }
-    /**
-     * End Helper Functions
-     * Begin Main
-     * 
-     */
+    document.addEventListener('scroll', setActiveSection);
+}
+/**
+ * End Helper Functions
+ * Begin Main
+ * 
+ */
 
-    // build the nav
-    buildNavMenu(getSectionList());
+// build the nav
+buildNavMenu(getSectionList());
 
 /**
  * End Main
