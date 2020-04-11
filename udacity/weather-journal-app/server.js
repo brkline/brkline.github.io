1fccb09c459f5017c320b33a3df26ce1b5969100
brkline <brkline@users.noreply.github.com>
2020-04-10 21:28:38 -0400

    Started building routes and server config

--- udacity/weather-journal-app/server.js
+++ udacity/weather-journal-app/server.js
@@ -2,18 +2,36 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
+const bodyParser = require('body-parser')
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+const cors = require('cors');
+app.use(cors());
 
 // Cors for cross origin allowance
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+const port = 8001;
+const server = app.listen(port, listening);
+function listening() {
+    console.log(`running on localhost: ${port}`)
+}
+
+// GET route
+app.get('/all', sendData);
+function sendData(request, response) {
+    response.send(projectData);
+}
+
+// POST route
+app.post('/add')
--- udacity/weather-journal-app/website/app.js
+++ udacity/weather-journal-app/website/app.js
@@ -1,4 +1,6 @@
 /* Global Variables */
+let baseURL = 'api.openweathermap.org';
+let apiKey = '&APPID=0c10360aec2b8a1a6c5c1e9ce3590ee7';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
